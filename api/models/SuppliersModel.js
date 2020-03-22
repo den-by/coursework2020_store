d17a17a89a01c00b0607f28d8879dd37b53d252b
Denis <denis.da.by@gmail.com>
2020-03-22 12:45:32 +0300

    added join statement

--- api/models/SuppliersModel.js
+++ api/models/SuppliersModel.js
@@ -1,5 +1,6 @@
-const TABLE_NAME = "supplier";
+const TABLE_NAME = "suppliers";
 const SUPPLIER_TYPE_ID = "supplier_type_id";
+const PRODUCT_ID = "product_id";
 
 class SuppliersModel extends require("./BaseModel") {
     static async get(req) {
@@ -13,13 +14,19 @@ class SuppliersModel extends require("./BaseModel") {
         let sql = `SELECT * FROM ${TABLE_NAME}`;
 
         let where = [];
+        let join = [];
 
         if (req.query[SUPPLIER_TYPE_ID]) {
             where.push(`${TABLE_NAME}.${SUPPLIER_TYPE_ID} = ${req.query[SUPPLIER_TYPE_ID]}`);
         }
 
-        if (req.query[SUPPLIER_TYPE_ID]) {
-            where.push(`${TABLE_NAME}.${SUPPLIER_TYPE_ID} = ${req.query[SUPPLIER_TYPE_ID]}`);
+        if (req.query[PRODUCT_ID]) {
+            join.push("JOIN links_products_suppliers on links_products_suppliers.supplier_id = suppliers.id");
+            join.push("JOIN products on products.id = links_products_suppliers.product_id");
+        }
+
+        if (join.length > 0) {
+            sql += ` ${join.join(" ")}`;
         }
 
         if (where.length > 0) {
