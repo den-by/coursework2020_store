b7851ba535bf1a61b52d9af2aa7857c9f04767b6
Denis <denis.da.by@gmail.com>
2020-04-28 20:08:01 +0300

    added total count into preOrder view

--- api/services/PreOrdersService.js
+++ api/services/PreOrdersService.js
@@ -4,17 +4,26 @@ const preOrdersModel = require('../models/PreOrdersModel');
 class PreOrdersService extends require("./BaseService") {
 
     static async getAll() {
-        preOrdersModel.joinClients().selectTable();
-        return preOrdersModel.getSQL();
+        preOrdersModel
+            .joinClients()
+            .selectTable();
+        return preOrdersModel
+            .getSQL();
     }
 
     static async getCount() {
-        const request = await preOrdersModel.setShowDefaultTable(false).selectCount().getSQL();
+        const request = await preOrdersModel
+            .setShowDefaultTable(false)
+            .selectCount()
+            .getSQL();
         return request[0];
     }
 
     static async getSumTotalPrice() {
-        const request = await preOrdersModel.setShowDefaultTable(false).selectSumTotalPrice().getSQL();
+        const request = await preOrdersModel
+            .setShowDefaultTable(false)
+            .selectSumTotalPrice()
+            .getSQL();
         return request[0];
     }
 
