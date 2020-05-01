51394dedf30e2c036c736e56e9491dac07646975
Denis <denis.da.by@gmail.com>
2020-05-01 20:53:43 +0300

    added calculated total count and total price into sales

--- api/controllers/SalesController.js
+++ api/controllers/SalesController.js
@@ -6,8 +6,14 @@ class SalesController extends require('./BaseController') {
 
     static async get(req, res) {
         const sales = await linksOrdersProductsService.getCashReportByDate(req.query[START_DATE], req.query[END_DATE]);
+        const count = await linksOrdersProductsService.getSumCount(req.query[START_DATE], req.query[END_DATE]);
+        const sumTotalPrice = await linksOrdersProductsService.getSumTotalPrice(req.query[START_DATE], req.query[END_DATE]);
 
-        res.render('sales', {title: 'Greetings form Handlebars', data: {sales: sales}, query: req.query})
+        res.render('sales', {
+            title: 'Greetings form Handlebars',
+            data: {sales: sales, ...count, ...sumTotalPrice},
+            query: req.query
+        })
     }
 }
 
--- api/services/LinksOrdersProductsService.js
+++ api/services/LinksOrdersProductsService.js
@@ -3,7 +3,6 @@ const linksOrdersProductsModel = require('../models/LinksOrdersProductsModel');
 
 class LinksOrdersProductsService extends require("./BaseService") {
 
-
     static async getCashReportByDate(startDate, endDate) {
         linksOrdersProductsModel
             .filterByStartDateAdd(startDate)
@@ -14,7 +13,7 @@ class LinksOrdersProductsService extends require("./BaseService") {
             .selectTable();
         return linksOrdersProductsModel
             .getSQL();
-    };
+    }
 
     static async getSumCountAndTotalPrice(startDate, endDate) {
         linksOrdersProductsModel
@@ -25,8 +24,27 @@ class LinksOrdersProductsService extends require("./BaseService") {
             .filterByEndDateAdd(endDate);
         return await linksOrdersProductsModel
             .getSQL();
-    };
+    }
 
+    static async getSumTotalPrice(startDate, endDate) {
+        const request = await linksOrdersProductsModel
+            .filterByStartDateAdd(startDate)
+            .filterByEndDateAdd(endDate)
+            .setShowDefaultTable(false)
+            .selectSumTotalPrice()
+            .getSQL();
+        return request[0];
+    }
+
+    static async getSumCount(startDate, endDate) {
+        const request = await linksOrdersProductsModel
+            .filterByStartDateAdd(startDate)
+            .filterByEndDateAdd(endDate)
+            .setShowDefaultTable(false)
+            .selectSumCount()
+            .getSQL();
+        return request[0];
+    }
 }
 
 module.exports = LinksOrdersProductsService;
